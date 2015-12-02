026de2cfbf5a882962728d64a62b21098bac120a
Alpvax <alpvax@netscape.net>
2015-12-02 18:01:18 +0000

    Added OS checking

--- Python/updater.py
+++ Python/updater.py
@@ -1,5 +1,15 @@
-import requests
+import requests, json, os
+from platform import system
 
-if __Name__ == "__main__":
-    r = requests.get("https://raw.github.com/Alpvax/PortableScriptUpdater/scripts.json")
-    print(r.text)#XXX
+if __name__ == "__main__":
+    platform = system()
+    datapath = os.path.expanduser("~/.alpUpdater")
+    userData = {}
+    if os.path.isfile(datapath):
+        with open(datapath, 'r') as file:
+            userData = json.load(file)
+    print(userData)
+    r = requests.get("https://raw.githubusercontent.com/Alpvax/PortableScriptUpdater/master/scripts.json")
+    for key, script in json.loads(r.text).items():
+        if script[platform]: #Check script has a version for this OS
+            print(script[platform])
